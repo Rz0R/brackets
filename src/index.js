4223de8dd08c7506f25110f881065918e2a1c777
Rz0R <vpa05@mail.ru>
2021-08-12 22:07:29 +1000

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+
+  const OPEN_BRACKETS = [];
+  const BRACKETS_PAIR = {};
+
+  for (const brackets of bracketsConfig) {
+    OPEN_BRACKETS.push(brackets[0]);
+    BRACKETS_PAIR[brackets[1]] = brackets[0];
+  }
+
+  let stack = [];
+
+  for (const ch of str) {
+    if (OPEN_BRACKETS.includes(ch) && ch === BRACKETS_PAIR[ch]) {
+      if (ch === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        stack.push(ch);
+      }
+    } else if (OPEN_BRACKETS.includes(ch) && ch !== BRACKETS_PAIR[ch]) {
+      stack.push(ch);
+    } else if (!OPEN_BRACKETS.includes(ch) && ch !== BRACKETS_PAIR[ch]) {
+      if (!stack.length) {
+        return false;
+      }
+      if (BRACKETS_PAIR[ch] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return !stack.length;
 }
+
+module.exports = check;
